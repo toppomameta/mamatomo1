d54deb2f999b028ff8e0f10f1fd8a8c2d4ba0931
toppomameta <toppo850122@gmail.com>
2018-03-23 08:24:43 +0900

    login画面カスタマイズ分

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180322081406) do
+ActiveRecord::Schema.define(version: 20180322231622) do
 
   create_table "authors", force: :cascade do |t|
     t.string "name"
@@ -41,6 +41,8 @@ ActiveRecord::Schema.define(version: 20180322081406) do
     t.string "last_sign_in_ip"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "family_name"
+    t.string "given_name"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
